5cb7d0b25f67582faa29f99b0675cd58e8d4ce88
thamy <thamyouwei@gmail.com>
2023-05-05 10:47:09 +0800

    drop down functionality

--- app/src/main/java/com/example/rideringer/BusFragment.java
+++ app/src/main/java/com/example/rideringer/BusFragment.java
@@ -1,5 +1,6 @@
 package com.example.rideringer;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,31 +9,55 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
 public class BusFragment extends Fragment {
-
+    private String[] mrt= {"Woodlands", "Yishun", "Choa Chu Kang"};
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> adapterItems;
+    private Database db;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_bus, container, false);
+        this.db = new Database(getActivity());
         v.findViewById(R.id.bus_save).setOnClickListener(onSave);
         v.findViewById(R.id.bus_alarm).setOnClickListener(onAlarm);
+        autoCompleteTextView = v.findViewById(R.id.bus_drop_list);
+        adapterItems = new ArrayAdapter<>(getContext(), R.layout.drop_down_item, mrt);
+        autoCompleteTextView.setAdapter(adapterItems);
+        autoCompleteTextView.setOnItemClickListener(onClick);
         return v;
+    }
+
+    @Override
+    public void onDestroyView() {
+        db.close();
+        super.onDestroyView();
     }
 
     private View.OnClickListener onSave = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Toast.makeText(getActivity().getApplicationContext(), "Bus save", Toast.LENGTH_LONG).show();
+            //db.insert(nameStr, addrStr);
         }
     };
 
     private View.OnClickListener onAlarm = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Toast.makeText(getActivity().getApplicationContext(), "Bus alarm", Toast.LENGTH_LONG).show();
+            //Set Alarm
+        }
+    };
+
+    private AdapterView.OnItemClickListener onClick = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String item = parent.getItemAtPosition(position).toString();
         }
     };
 }
--- app/src/main/java/com/example/rideringer/MRTFragment.java
+++ app/src/main/java/com/example/rideringer/MRTFragment.java
@@ -5,38 +5,57 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.Toast;
-
-import com.google.android.gms.common.logging.Logger;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MRTFragment extends Fragment {
+    private String[] mrt= {"Woodlands", "Yishun", "Choa Chu Kang"};
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> adapterItems;
+    private Database db;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_m_r_t, container, false);
+        this.db = new Database(getActivity());
         v.findViewById(R.id.mrt_save).setOnClickListener(onSave);
         v.findViewById(R.id.mrt_alarm).setOnClickListener(onAlarm);
+        autoCompleteTextView = v.findViewById(R.id.mrt_drop_list);
+        adapterItems = new ArrayAdapter<>(getContext(), R.layout.drop_down_item, mrt);
+        autoCompleteTextView.setAdapter(adapterItems);
+        autoCompleteTextView.setOnItemClickListener(onClick);
         return v;
+    }
+
+    @Override
+    public void onDestroyView() {
+        db.close();
+        super.onDestroyView();
     }
 
     private View.OnClickListener onSave = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Toast.makeText(getActivity().getApplicationContext(), "MRT save", Toast.LENGTH_LONG).show();
+            //db.insert(nameStr, addrStr);
         }
     };
 
     private View.OnClickListener onAlarm = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Toast.makeText(getActivity().getApplicationContext(), "MRT alarm", Toast.LENGTH_LONG).show();
+            //Set Alarm
+        }
+    };
+
+    private AdapterView.OnItemClickListener onClick = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String item = parent.getItemAtPosition(position).toString();
         }
     };
 }
