0f28c7dbfacf957d6ca1a1fa376da6c6ec9825a7
thamy <thamyouwei@gmail.com>
2023-05-31 22:53:02 +0800

    Setting up of SharedPreferences and also completed SettingsActivity

--- app/src/main/java/com/example/rideringer/SettingsActivity.java
+++ app/src/main/java/com/example/rideringer/SettingsActivity.java
@@ -2,21 +2,72 @@ package com.example.rideringer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
     private Switch alarm;
     private Switch banner;
     private Switch tracker;
+    private UserSettings settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        settings = (UserSettings) getApplication();
+
         alarm = findViewById(R.id.switch_alarm);
         banner = findViewById(R.id.switch_banner);
         tracker = findViewById(R.id.switch_tracker);
+
+        initSwitchListener();
+    }
+
+    private void initSwitchListener() {
+        banner.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = getSharedPreferences(UserSettings.PREFERENCES, MODE_PRIVATE).edit();
+                editor.putBoolean(UserSettings.NOTIFICATION_SETTINGS, isChecked);
+                editor.apply();
+//                SharedPreferences test = getSharedPreferences(UserSettings.PREFERENCES,MODE_PRIVATE);
+//                boolean bool = test.getBoolean(UserSettings.NOTIFICATION_SETTINGS, true);
+//                String bool2 = new Boolean(bool).toString();
+//                Toast.makeText(getApplicationContext(), bool2, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        alarm.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = getSharedPreferences(UserSettings.PREFERENCES, MODE_PRIVATE).edit();
+                editor.putBoolean(UserSettings.ALARM_SETTINGS, isChecked);
+                editor.apply();
+//                SharedPreferences test = getSharedPreferences(UserSettings.PREFERENCES,MODE_PRIVATE);
+//                boolean bool = test.getBoolean(UserSettings.NOTIFICATION_SETTINGS, true);
+//                String bool2 = new Boolean(bool).toString();
+//                Toast.makeText(getApplicationContext(), bool2, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        /***************Leave it here first, Ive already made a GPS settings alert in GPS tracker***********/
+
+//        tracker.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                SharedPreferences.Editor editor = getSharedPreferences(UserSettings.PREFERENCES, MODE_PRIVATE).edit();
+//                editor.putBoolean(UserSettings.GPS_SETTINGS, isChecked);
+//                editor.apply();
+////                SharedPreferences test = getSharedPreferences(UserSettings.PREFERENCES,MODE_PRIVATE);
+////                boolean bool = test.getBoolean(UserSettings.NOTIFICATION_SETTINGS, true);
+////                String bool2 = new Boolean(bool).toString();
+////                Toast.makeText(getApplicationContext(), bool2, Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 }
