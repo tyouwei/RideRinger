c1766407cb0580f1dae6d9309b9255602e8dec78
thamy <thamyouwei@gmail.com>
2023-06-15 12:11:56 +0800

    Implemented skeleton code for Notifications and alarm feature

--- app/src/main/java/com/example/rideringer/AlarmReceiver.java
+++ app/src/main/java/com/example/rideringer/AlarmReceiver.java
@@ -1,10 +1,18 @@
 package com.example.rideringer;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
@@ -14,9 +22,34 @@ public class AlarmReceiver extends BroadcastReceiver {
         boolean useNotification = prefs.getBoolean(UserSettings.NOTIFICATION_SETTINGS, true);
 
         if (useNotification) {
+            Intent i = new Intent(context, AlarmActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, i, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
 
+            String channelId = "location_id";
+            Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, channelId)
+                    .setSmallIcon(android.R.drawable.stat_notify_chat)
+                    .setContentTitle("Ride Ringer")
+                    .setContentText("You have reached your destination. Please alight")
+                    .setAutoCancel(true)
+                    .setSound(defaultSoundUri)
+                    .setColor(ContextCompat.getColor(context, com.google.android.material.R.color.design_default_color_primary))
+                    .setContentIntent(pendingIntent);
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel(channelId,
+                        "Ride Ringer",
+                        NotificationManager.IMPORTANCE_DEFAULT);
+                notificationManager.createNotificationChannel(channel);
+            }
+
+            notificationManager.notify(0, notificationBuilder.build());
         } else if (useAlarm) {
-
+            Intent i = new Intent(context, AlarmActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
         }
     }
 }
